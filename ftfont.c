e2a8ee5367d3bc94be94ef4897f873f1f180d1a9
Nils O. Selåsdal <nos@utel.tech>
2026-05-25 00:07:22 +0200

    Free tex memory

--- ftfont.c
+++ ftfont.c
@@ -163,7 +163,7 @@ FTFont *load_font(LArena *arena, const char *filename)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA8, width, height, 0, GL_ALPHA, GL_UNSIGNED_BYTE, tex_data);
     // And delete the original memory block
-    //larena_tmp_end(&temp_tex_data);
+    larena_tmp_end(&temp_tex_data);
     fclose(in);
 
     return f;
